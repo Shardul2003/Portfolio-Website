0a0f7c05407151d4971bd020bad28b58487ff5fb
Shardul Dhongade <shardul21@vt.edu>
2024-08-26 03:13:49 -0400

    Clean up email code

--- app/components/EmailSection.jsx
+++ app/components/EmailSection.jsx
@@ -6,34 +6,6 @@ import Link from 'next/link';
 import Image from 'next/image';
 
 const EmailSection = () => {
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-//     const data = {
-//         email: e.target.email.value,
-//         subject: e.target.subject.value,
-//         message: e.target.message.value,
-//     }
-//     const JSONdata = JSON.stringify(data);
-//     const endpoint = "/api/send";
-
-//     const options = {
-//         method: 'POST',
-
-//         headers: {
-//             'Content-Type': 'application/json',
-//         },
-
-//         body: JSONdata,
-//     }
-
-//     const response = await fetch(endpoint, options);
-//     const resData = await response.json();
-//     console.log(resData);
-
-//     if(response.status === 200){
-//         console.log('Message sent');
-//     }
-//   }
   return (
     <section className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative">
         <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-purple-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2">
